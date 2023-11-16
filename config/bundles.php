f1f038a6b61a634e82a9f90fea29d0eae32779ec
rkwadriga <dmitry.kushneriov@obreey-products.com>
2023-11-16 16:42:49 +0200

    Install "api-platform"

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
